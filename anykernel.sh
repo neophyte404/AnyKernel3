82a4c6439cec29889cede379ba097e44995a25db
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-05-27 03:59:52 +0000

    anumase

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # begin properties
 properties() { '
 kernel.string=
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
@@ -13,7 +13,7 @@ do.cleanuponabort=0
 device.name1=topaz
 device.name2=tapas
 device.name3=xun
-supported.versions=13 - 14.1
+supported.versions=
 '; } # end properties
 
 # shell variables
